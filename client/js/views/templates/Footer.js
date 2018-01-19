4c2578f71bc7b0ca8653a89f8aac3808eae45c07
AlexCadigan <cadiganalexander@gmail.com>
2018-01-19 14:21:13 -0500

    Completed draft of homepage header and footer

--- client/js/views/templates/Footer.js
+++ client/js/views/templates/Footer.js
@@ -1,14 +1,88 @@
-module.exports = function() { return `` +
-`<footer>
-    <div>
-        <span>Contact</span>
-        <span>
-            <a href="mailto:info@futuredays.io">info@futuredays.io</a>
-        </span>
+/**
+* Project: Future Days Farm Website
+* Created By: Chris Baron
+* Date Last Modified: 1/19/2018 by Alex Cadigan
+* Description: This file builds the footer elements on the Farm Website homepage
+*/
+
+module.exports = function() 
+{ 
+    return `<footer id = "footerBody">
+
+    <!-- Future Days Farm Info -->
+    <div id = "footerTitle" class = "footer">
+
+        FUTURE DAYS FARM
+
     </div>
-    <div>
-        <span>Copyright</span>
-        <span>${new Date().getFullYear()} FutureDays Software</span>
+
+    <div class = "footer">
+
+        2123 Tiny Road
+        <br>
+        Town Name, Michigan 33344        
+
     </div>
-</footer>`
+
+    <br>
+
+    <div class = "footer">
+
+        <a id = "footerLink" href = "mailto:Info@FutureDaysFarm.com"> Info@FutureDaysFarm.com </a>
+        <br>
+        (333) 323-8899
+
+    </div>
+
+    <br>
+
+    <!-- Copyright -->
+    <div id = "footerBottom" class = "footer">
+
+        Copyright ${ new Date().getFullYear() } FutureDays Software
+
+    </div>
+
+    <style>
+
+        @import url('https://fonts.googleapis.com/css?family=Roboto');
+        @import url('https://fonts.googleapis.com/css?family=Arvo');
+
+        /**
+        * Body of footer
+        */
+        #footerBody
+        {
+            background-color: #C49542;
+        }
+
+        /**
+        * Future Days Farm Info
+        */
+        .footer
+        {
+            text-align: center;
+            font-family: Roboto;
+            color: #231F20;
+        }
+        #footerTitle
+        {
+            padding-top: 40px;
+            font-family: Arvo;
+            color: white;
+            font-weight: bold;
+        }
+        #footerLink
+        {
+            color: #231F20;
+            text-decoration: none;
+        }
+        #footerBottom
+        {
+            padding-bottom: 40px;
+        }
+
+    </style>
+
+    </footer>`
 }
--- client/js/views/templates/Home.js
+++ client/js/views/templates/Home.js
@@ -1,1 +1,8 @@
-module.exports = function( { model } ) { return `<div>Welcome Home</div>` }
+/**
+* Project: Future Days Farm Website
+* Created By: Chris Baron
+* Date Last Modified: 1/19/2018 by Alex Cadigan
+* Description: This file builds the body of the page (the main content)
+*/
+
+module.exports = function( { model } ) { return `<div>Home page stuff goes here</div>` }
